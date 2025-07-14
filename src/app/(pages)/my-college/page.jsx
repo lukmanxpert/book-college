8b78b6fda3dd79310638664adb8bf32572a61264
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-14 10:23:20 +0600

    added a part of my college page

--- src/app/(pages)/my-college/page.jsx
+++ src/app/(pages)/my-college/page.jsx
@@ -1,7 +1,26 @@
+import { Button } from '@/components/ui/button';
+import { auth } from '@/lib/auth'
+import Link from 'next/link';
 import React from 'react'
 
-export default function MyCollege() {
+export default async function MyCollege() {
+  const session = await auth()
+  console.log('session :>> ', session);
+  const admitCollegeStatus = session.user.admittedCollege.collegeId
   return (
-    <div>MyCollege</div>
+    <div className='min-h-[70dvh]'>
+      {
+        admitCollegeStatus && (
+          <div className='min-h-[70dvh] flex flex-col items-center justify-center gap-4'>
+            <h3 className="scroll-m-20 text-2xl text-center font-semibold tracking-tight">
+              You do not admitted any college yet.
+            </h3>
+            <Link href={"/admission"} className='cursor-pointer text-center'>
+              <Button className={"cursor-pointer"} variant={"outline"}>Admit Now</Button>
+            </Link>
+          </div>
+        )
+      }
+    </div>
   )
 }
